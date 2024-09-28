c7cd5eadf6914a90ebec13042cc6cf311819620b
Joko Adi S <user@USERs-MacBook-Air.local>
2024-09-28 14:10:05 +0700

    deploy

--- app/edit/[id]/page.tsx
+++ app/edit/[id]/page.tsx
@@ -3,9 +3,9 @@ import { getImageById } from "@/lib/data"
 import { notFound } from "next/navigation"
 
 
-const EditPage = async ({paramas}:{paramas:{id:string}}) => {
+const EditPage = async ({params}:{params:{id:string}}) => {
 
-    const data = await getImageById(paramas.id)
+    const data = await getImageById(params.id)
     if (!data) {
         return notFound()
     }
